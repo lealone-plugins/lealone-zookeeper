ec47fe582a119b0a4f4f7b0317716ab8a403f46e
Honghua Zhu <zhh200910@gmail.com>
2023-11-09 08:17:13 +0800

    已经在ProtocolServerEngineBase中统一负责各种api的实现

--- src/main/java/org/lealone/plugins/zookeeper/server/ZooKeeperServerEngine.java
+++ src/main/java/org/lealone/plugins/zookeeper/server/ZooKeeperServerEngine.java
@@ -5,32 +5,19 @@
  */
 package org.lealone.plugins.zookeeper.server;
 
-import java.util.Map;
-
 import org.lealone.server.ProtocolServer;
 import org.lealone.server.ProtocolServerEngineBase;
 
 public class ZooKeeperServerEngine extends ProtocolServerEngineBase {
 
     public static final String NAME = "ZooKeeper";
-    private final ZooKeeperServer server = new ZooKeeperServer();
 
     public ZooKeeperServerEngine() {
         super(NAME);
     }
 
     @Override
-    public ProtocolServer getProtocolServer() {
-        return server;
-    }
-
-    @Override
-    public void init(Map<String, String> config) {
-        server.init(config);
-    }
-
-    @Override
-    public void close() {
-        server.stop();
+    protected ProtocolServer createProtocolServer() {
+        return new ZooKeeperServer();
     }
 }
